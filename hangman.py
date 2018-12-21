c37b57009ba11e170b3e0cd4c7ced3b36ffb08c6
Robert Palazzo <rpalazzo@gmail.com>
2018-12-21 07:43:51 -0500

    refactored lists and strings

--- hangman.py
+++ hangman.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 
 import random
 import re
@@ -17,40 +18,40 @@ while True:
 #print(secret_word)     # debug
 #print(tries)           # debug
 
-guess_display = ["_ " for i in range(len(secret_word))]
 
-def print_guess_display():
-    display = ""
-    for i in range(len(secret_word)):
-        display += guess_display[i]
-        display += ' '
-    print(display)
+def list_to_stretched_string(lst):
+    str = ""
+    for i in range(len(lst)):
+        str += lst[i]
+        str += ' '
+    return(str)
 
-
+secret_display = ["_" for i in range(len(secret_word))]
+guessed_letters = []
 strikes = 0
-guessed_letters = ""
 
-
-while strikes < 10 and "_ " in guess_display:
+while strikes < 10 and "_" in secret_display:
     print("")
-    print_guess_display()
-    print("guesses: ", guessed_letters)
+    print(list_to_stretched_string(secret_display))
+    print("Guesses: ", list_to_stretched_string(guessed_letters))
     print("Strikes: ", strikes)
 
     guess = input("Guess a letter: ")
-    guessed_letters += guess
+    guessed_letters.append(guess)
+    guessed_letters.sort()
     if guess in secret_word:
         for i in range(len(secret_word)):
             if secret_word[i] == guess:
-                guess_display[i] = guess
+                secret_display[i] = guess
     else:
         strikes += 1
         print("\nNOPE! Strike ", strikes)
 
 if strikes == 10:
     print("Game over: You got hung")
+    print("The word was ", secret_word)
 else:
     print("Game over: You WIN!")
 
-print(secret_word)
+
 
